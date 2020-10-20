564ccee61d414fc42c2fb37b9a3b79d97ec052c4
palacionis <jonas.palacionis@gmail.comm>
2020-10-20 14:13:35 +0300

    adding fifth

--- Chapter_5:Files and I:O/chapter_5.py
+++ Chapter_5:Files and I:O/chapter_5.py
@@ -45,3 +45,47 @@ with gzip.open('somefile.gz', 'rt') as f: # wt to write
 import bz2
 with bz2.open('somefile.gz', 'rt') as f:
     text = f.read()
+
+
+# ---------------------------------------------------------------------------
+
+
+# Problem_4 : iterating over fixed-sized records
+
+# Solution : using iter function anf functools.partial() method 
+
+from functools import partial
+RECORD_SIZE = 32
+
+with open('data.bin', 'rb') as f:
+    records = iter(partial(f.read, RECORD_SIZE), b'')
+    for r in records:
+        print(r)
+
+
+# ---------------------------------------------------------------------------
+
+
+# Problem_5 : getting directory listing
+
+# Solution : using os and glob modules
+
+
+
+import os
+import os.path
+import glob
+
+pyfiles = glob.glob('*.py')
+
+# Get file sizes and modification dates
+name_sz_date = [(name, os.path.getsize(name), os.path.getmtime(name))
+                for name in pyfiles]
+
+for r in name_sz_date:
+    print(r)
+
+# Get file metadata
+file_metadata = [(name, os.stat(name)) for name in pyfiles]
+for name, meta in file_metadata:
+    print(name, meta.st_size, meta.st_mtime)
